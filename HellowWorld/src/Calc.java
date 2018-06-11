8463fb0c7f005a5ad14347261cede88bb0f0c184
Kkenichi <gucci_74210_ken@hotmail.com>
2018-06-11 21:45:36 +0900

    firstcommit:java_commit_pra

---
+++ HellowWorld/src/Calc.java
@@ -0,0 +1,15 @@
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 16;
+		int b = 4;
+		int total = CalcLogic.tasu(a,b);
+		System.out.println(total);
+
+		int delta = CalcLogic.hiku(a, b);
+		System.out.println(delta);
+	}
+
+}
