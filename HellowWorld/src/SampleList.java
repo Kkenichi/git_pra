3e3b4af16f4a5c4c4a645e14348c283be4c5650b
Kkenichi <gucci_74210_ken@hotmail.com>
2018-06-24 18:21:28 +0900

    R:firstcommit

---
+++ HellowWorld/src/SampleList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleList {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		List<String> aaList = new ArrayList<>();
+		aaList.add("a");
+		aaList.add(null);
+		System.out.println(aaList);
+		conatString(aaList);
+	}
+	public static void conatString(List<String> aaList) {
+		String a = "1111";
+		for(int i =0; i < aaList.size(); i++) {
+			a = a + aaList.get(i);
+		}
+		System.out.println(a);
+
+	}
+}
